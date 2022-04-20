3a28456f87eba14c340b5a4687ff3e4d413228fd
Anders Hovmöller <anders.hovmoller@dryft.se>
2022-04-20 03:16:09 +0200

    Initial Commit

---
+++ BoxedDock/BoxedDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxedDockApp.swift
+//  BoxedDock
+//
+//  Created by Anders Hovmöller on 2022-04-20.
+//
+
+import SwiftUI
+
+@main
+struct BoxedDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoxedDock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoxedDock
+//
+//  Created by Anders Hovmöller on 2022-04-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
